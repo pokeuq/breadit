956acfa5fed6adf940fe24ed423541ab970d3411
pokeuq <109028724+pokeuq@users.noreply.github.com>
2024-06-29 18:06:41 +0500

    </>

--- src/app/global/page.tsx
+++ src/app/global/page.tsx
@@ -1,15 +1,17 @@
 import GeneralFeed from "@/components/homepage/GeneralFeed";
+import { getAuthSession } from "@/lib/auth";
 
-const page = () => {
+const page = async () => {
+  const session = await getAuthSession();
   return (
     <>
       <h1 className="font-bold text-3xl md:text-4xl">Global feed</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
         {/* @ts-expect-error server component */}
-        <GeneralFeed />
+        {session && <GeneralFeed />}
       </div>
     </>
   );
-}
+};
 
-export default page
+export default page;
